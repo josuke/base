f3f731df583f3cf280034d24a7deae679b30bc6c
Josuke <josuke@dixital.com>
2026-02-03 13:46:47 +0100

    Release 0.1.33

--- page-wip.php
+++ page-wip.php
@@ -3,6 +3,13 @@
 Template Name: WIP
 */
 ?>
-<?php while (have_posts()) : the_post(); ?>
-	<?php the_content(); ?>
-<?php endwhile; ?>
+
+<?php get_header(); ?>
+
+<main>
+	<?php while (have_posts()) : the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
